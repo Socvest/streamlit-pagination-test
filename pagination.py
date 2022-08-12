226a8c759d83c5e9d7910989d1d61113232b8361
Socvest <82335390+Socvest@users.noreply.github.com>
2022-08-12 15:55:04 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -19,7 +19,7 @@ num_of_rows = st.slider("number of columns", 0,10000, value=1000)
 num_of_row_chunks = st.number_input("Number of rows per chunk of data", value=100)
 
 
-data = pd.DataFrame(np.random.randint(0,100,size=(1000, num_of_cols)), columns=list(random_char(num_of_cols))) 
+data = pd.DataFrame(np.random.randint(0,100,size=(num_of_rows, num_of_cols)), columns=list(random_char(num_of_cols))) 
 
 n = num_of_row_chunks  
 list_df = [data[i:i+n] for i in range(0,data.shape[0],n)] 
