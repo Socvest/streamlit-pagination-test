872aaa9eddaf121e015cabfc0c26b7b625cd265d
Socvest <82335390+Socvest@users.noreply.github.com>
2022-08-12 16:16:09 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -14,7 +14,7 @@ if 'foo' not in st.session_state:
 def random_char(y):
     alphabet = list(string.ascii_lowercase)
     random.shuffle(alphabet)
-    return ''.join(alphabet for x in range(y))
+    return ''.join(random.choice(alphabet for x in range(y)))
 
 num_of_cols = st.slider("number of columns", 0,100, value=4)
 num_of_rows = st.slider("number of rows", 0,10000, value=1000)
