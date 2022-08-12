985b6c6c791289e2539b15f2d37c36e997b85989
Socvest <82335390+Socvest@users.noreply.github.com>
2022-08-12 16:53:58 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -116,7 +116,7 @@ def random_char(y):
     
     return words[:y]
 
-with st.expander:
+with st.expander("Make adjustments to data"):
 
     num_of_cols = st.slider("number of columns", 0,100, value=4)
     num_of_rows = st.slider("number of rows", 0,10000, value=1000)
